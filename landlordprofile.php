62cda44054249daa9891b8b8dab8d9a958dc84fd
ogbenjamin <oligbenjamin@gmail.com>
2015-03-03 22:58:27 -0600

    Create landlordprofile.php

---
+++ landlordprofile.php
@@ -0,0 +1,137 @@
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+Creator : Olivia Benjamin 
+-->
+<!doctype html>
+<html>
+<head>
+<title>Register</title>
+<link rel='stylesheet' type='text/css' href='styles/Landlordstyle.css'</link>
+<!-- JQUERY AND AJAX UPLOAD STARTS HERE -->
+<script src="/js/jquery.min.js" type="text/javascript"></script>
+<script src="/js/ajaxupload.js" type="text/javascript"></script>
+<!-- JQUERY AND AJAX UPLOAD STARTS HERE -->
+</head>
+<body>
+<center>
+<div id="wrapper"> 
+ <!-- ============ MENU STARTS  ============== -->
+    <div id="menu">
+		<ul>
+                    <li><a href="HomePage.php">Home</a></li>
+                    <li class = "last"><a href="#">Preview Page</a></li>
+                    <li class = "last"><a href="LogoutPage.php">Logout</a></li>
+                    
+                </ul>
+	</div>
+<!-- ============ TABLES STARTS  ============== -->
+<table width="100%" style="height: 95%;" cellpadding="10" cellspacing="0" border="0">
+    
+<td colspan="3" style="height: 200px;" bgcolor="#E0E0D1">
+  <a href=# align="left"><small>Change Logo Image</small></a>
+   <center><img  src="https://imageshack.com/i/p1sX7984j" alt="Complex picture" height="250" width="1170" > </center>   
+   
+</td></tr>
+    
+<tr>
+
+<!-- ============ LEFT COLUMN  ============== -->
+<td width="20%" valign="top" bgcolor="#C299C2">
+
+
+
+<h4>Welcome back</h4><h2>Name</h2>
+
+                                                <p>Apartment Address  
+                                                 <a href=#><small><u>edit info</u></small></a> 
+                                                </p>
+                                                <p>Business Number 
+                                                 <a href=#><small><u>edit info</u></small></a> 
+                                                </p>
+                                                <p>Contact Email 
+                                                 <a href=#><small><u>edit info</u></small></a> 
+                                                </p>
+                                                <p>Business Hours
+                                                 <a href=#><small><u>edit info</u></small></a> 
+                                                </p>
+                                                <p><a href="#" <size = small class="link-style">Publish</a></p>
+</td>
+
+<!-- ============ RIGHT COLUMN (CONTENT) ============== -->
+<td width="50%" valign="top" bgcolor="#E0E0D1">
+<!-- floor plans edit information starts here -->
+<center><h2>Floor Plans</h2></center>
+<div 
+<ul class=floorplan>
+    <p><h4>1 bedroom  1 bath Suite</h4>
+    Cost : $585.00 + Utilities<br>
+    <p><h4>Utilities include:</p></h4>
+        <li>Water</li>
+        <li>Electricity</li><ul> <a href=#><small><u>edit info</u></small></a> </p>
+<!-- floor plans edit information stops here -->
+
+<!-- floor plans image information starts here -->
+        <center><img  src="https://imageshack.com/i/p1HthunXj" alt="profilepic" height="350" width="350" border="5">
+ <br>
+  <a href=#><small><u>Change floor plan image</u></small></a></center>
+ </p>
+ <!-- floor plans image information stops here -->
+ 
+ 
+<!-- floor plans add image/information starts here -->
+ 
+<a href=#><small><u>Add More Floor Plan Info</u></small></a><br>
+<a href=#><small><u>Add More Floor Plan Image</u></small></a> <br>
+ <a href=#><small><u>Add Slide Show</u></small></a> <br>
+<!-- floor plans image information stops here -->
+ 
+ <br></div>
+<p><a href="#" <size = small class="link-style">Publish</a></p>
+</td>
+
+
+<!-- ============ THIRD RIGHT COLUMN (CONTENT) ============== -->
+<td width="30%" valign="top" bgcolor="#C299C2">&nbsp;
+
+<center><h2> Apartment Documents </h2></center>
+ <br> 
+ <br>
+<p>Leasing Information <a href=#><small><u>Edit Title</u></small></a>
+     <br>
+ <a href=#><small><u>upload</u></small></a> 
+ </p>
+ <p>Leasing Information <a href=#><small><u>Edit Title</u></small></a>
+     <br>
+ <a href=#><small><u>upload</u></small></a> 
+ </p>
+ <p>Leasing Information <a href=#><small><u>Edit Title</u></small></a>
+     <br>
+ <a href=#><small><u>upload</u></small></a> 
+ </p>
+ <p>Leasing Information <a href=#><small><u>Edit Title</u></small></a>
+     <br>
+ <a href=#><small><u>upload</u></small></a> 
+ </p>
+ <br> 
+<p><a href="#" <size = small class="link-style">Publish</a></p>
+</td>
+
+</tr>
+
+
+<!-- ============ FOOTER SECTION ============== -->
+<tr><td colspan="3" align="center" height="20" bgcolor="#E6CC80">All rights reserved.<br> PVCOMMUNITY </td></tr>
+</table>
+
+        <br>
+      
+
+<head>
+<title>lANDLORD PAGE</title>
+
+</head>
+</body>
