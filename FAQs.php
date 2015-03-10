003004108efb6d8c7e820b96c8986290519e7045
jalisap <jalisaperkins10@gmail.com>
2015-03-10 16:09:46 -0500

    faqs and quiz
    styles and code

--- FAQs.php
+++ FAQs.php
@@ -1,150 +1,208 @@
 <?php
 echo "
 <!DOCTYPE html>
-<html lang='en-US'>
+<html lang='en'>
+
 <head>
-
     <meta charset='utf-8'>
-    <meta http-equiv='X-Ua-Compatible' content='IE=edge'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <meta name='description' content='FAQs'>
-    <meta name='author' content='LISa P'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
 
-<link rel='stylesheet' type='text/css' href='styles/stylesheettest.css'</link>
-<title> Frequently Asked Questions </title>
-<link rel='stylesheet' type='text/css' href='styles/style.css'></link>
-<link rel='stylesheet' type='text/css' href='styles/faqs.css'></link>
-<link rel='stylesheet' type='text/css' href='styles/ui-lightness/jQuery-ui-1.8.14.custom.css'></link>
+<center> <link rel='stylesheet' type='text/css' href='styles/FAQs.css'</link>
 
-</head>";
+<!-- jQuery -->
+    <script src='js/jquery.js'></script>
 
-echo "
-<br>
-<br>
-<div id='container' align='center'>
-<center> 
-<div class='menu'>
-      
-<a href='index.php'>Home</a>
-<a href='login.php'>Login</a>
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<div class='container'>
+<div id='wrapper'> 
+    <div id='logo'>
+       
                   
+                    <br>
+                    <br>
+                    <br>
+                      <center><h1><a href='#'>PV Student Community</a></h1></center>       
+					<br>
+					<br>
+                    <br>
 </div>
-<div class='background'>
+	<div id='menu'>
+		<ul>
+			<li class='current_page_item'><a href='index.php'>Home</a></li>
+			<li><a href='login.php'>Login</a></li>
+			<li class ='last'><a href=#>Question/Concerns</a></li>
+		</ul>
+	</div>
+	<center>
+			<br>
+			<img src='images/FAQs.png'>
+	
+	
+<!-- QUESTIONS -->
+<!--Questions Style-->
 
-<br><br>
+<style>
+.content
+{
+	color: #ffffff;
+}
+.faq {	
+	background: #3f194a;
+	font-size: 1.6em;
+	
+}
 
-<big><strong>FAQs</strong></big>
-<br>
-<br>
-<a href='#1'>Why Can't I Register?</a>
-<br><br> 
-<a href='#2'>Are the properties only on campus?</a> 
-<br><br> 
-<a href='#3'>FaQ#3</a> 
-<br><br> 
-<a href='#4'>
+.label {
+	background: white;
+	
+}
 
-FaQ#4
-
-</a> 
-<br><br> 
-<a href='#5'>
-
-FaQ#5
-
-</a> 
-<br><br> 
-<a href='#6'>
-
-FaQ#6
-
-</a> 
-<br><br> 
-<a href='#7'>
-
-FaQ#7
-
-</a> 
-<br><br> 
-<a href='#8'>
-
-FaQ#8
-
-</a> 
-<br><br> 
-<a href='#9'>
-
-FaQ#9
-
-</a> 
-<br><br> 
-<a href='#10'>
-
-FaQ#10
-
-</a> 
-<br><br> 
-<a href='#11'>
-
-FaQ#11
-
-</a> 
-<br><br> 
-<a href='#12'>
-
-FaQ#12
-
-</a> 
-<br><br> 
-<a href='#13'>
-
-FaQ#13
-
-</a> 
-<br><br> 
-<a href='#14'>
-
-FaQ#14
-
-</a> 
-<br><br> 
-<a href='#15'>
-
-FaQ#15
-
-</a> 
-<br><br> 
+</style>
 
 
-<a name='1'>Why Can't I Register? <br>
-Only Students are able to register using their .pvamu email addresses.</a><br><br> 
-<a name='2'>Are the properties only on campus? <br>
-The properties are properties in the entire Prairie View area, not just on campus.</a><br><br> 
-<a name='3'>How can I change my profile picture?<br>
-On your student website, click the edit profile picture link that is located under the current profile picture</a><br><br> 
-<a name='4'>What does my suggested users section on my profile mean?<br>
-The suggested users are based on your likes and these are the individuals around you with like personalities and are possible roommate candidates.</a><br><br> 
-<a name='5'>Am I able to retake my personality quiz because I don't think that my personality answer is correct? <br>
-There are only two times available to take the personality quiz, but its best not to.</a><br><br> 
-<a name='6'>How can I contact the landlord properties?<br>
-You can contact them by visiting their profiles' contact information once you click on the desired property from your home page.</a><br><br> 
-<a name='7'>What does my personality quiz results mean?<br>
-Once on your personal profile, you can view the description of your personality result.</a><br><br> 
-<a name='8'>Can I use this website on my phone and find it fully functional?<br>
-Indeed you can. Just visit the website from a mobile browser and you will be redirected to the mobile version of the website.</a><br><br> 
-<a name='9'>Is this service free?<br>
-It is indeed free to use.</a><br><br> 
+<!-- Actual Questions -->
 
-<a href=#><img src='questions.jpg'></a>
+<div class='faq'>
+	<div id='first' class='label'>
+		<a href=#1>Why Can't I Register?</a>
+	</div>
+	<div class='content'>
+		<a name='1'><font color=white>Only Students are able to register using their @pvamu.edu email addresses.</font></a>
+	</div>
+</div>
 
 
-			<footer>
-                    <small>Copyright &copy; PV Student Community 2014</small>
-			</footer>
+
+<div class='faq'>
+	<div id='second' class='label'>
+		<a href=#2>Are the properties only on campus?</a>
+	</div>
+	<div class='content'>
+		<a name='2'><font color='white'>The properties are properties in the entire Prairie View area, not just on campus.</a></font>
+	</div>
+</div>
+
+
+
+<div class='faq'>
+	<div id='third' class='label'>
+		<a href=#3>How can I change my profile picture?</a>
+	</div>
+	<div class='content'>
+		<a name='3'><font color='white'>On your student website, click the edit profile picture link that is located under the current profile picture</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='fourth' class='label'>
+		<a href=#4>What does my suggested users section on my profile mean?</a>
+	</div>
+	<div class='content'>
+		<a name='4'><font color='white'>The suggested users are based on your likes and these are the individuals around you with like personalities and are possible roommate candidates.</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='fifth' class='label'>
+		<a href=#5>Am I able to retake my personality quiz because I don't think that my personality answer is correct?</a>
+	</div>
+	<div class='content'>
+		<a name='5'><font color='white'>There are only two times available to take the personality quiz, but its best not to.</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='sixth' class='label'>
+		<a href=#6>How can I contact the landlord properties?</a>
+	</div>
+	<div class='content'>
+		<a name='6'><font color='white'>You can contact them by visiting their profiles' contact information once you click on the desired property from your home page.</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='seven' class='label'>
+		<a href=#7>What does my personality quiz results mean?</a>
+	</div>
+	<div class='content'>
+		<a name='7'><font color='white'>Once on your personal profile, you can view the description of your personality result.</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='eighth' class='label'>
+		<a href=#8>Can I use this website on my phone and find it fully functional?</a>
+	</div>
+	<div class='content'>
+		<a name='8'><font color='white'>Indeed you can. Just visit the website from a mobile browser and you will be redirected to the mobile version of the website.</a></font>
+	</div>
+</div>
+
+<div class='faq'>
+	<div id='tenth' class='label'>
+		<a href=#9>Is this service free?</a>
+	</div>
+	<div class='content'>
+		<a name='9'><font color='white'>It is indeed free to use.</a></font>
+	</div>
+</div>
+
+	</center>
+	
+ </div>
+</div>
+</div>
+<!-- /.container -->
+
+	
+
+
+<!-- SCRIPT FOR QUESTIONS -->
+<script>
+	$('.content').hide();
+	$('.label').click(function()
+	{
+	
+		if ($('.content').is(':visible'))
+		{
+		$('.content').hide();
+		}
+		else
+		$('.content').show();
+	
+		$('.content').hide(); 
+		$(this).parent().children('.content').toggle();
+	});
+	
+</script>
+
+
+
+
+   <footer>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-lg-12 text-center'>
+                    <p>Copyright &copy; PV Student Community 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    
+
+    <!-- Bootstrap Core JavaScript 
+    <script src='js/bootstrap.min.js'></script>-->
 </center>
-
-</div>
-</div>
 </body>
 </html> ";
 ?>
---
+++ PersonalityQuiz.php
@@ -0,0 +1,98 @@
+<?php
+echo "
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+<center> 
+<!--Personality Quiz stylesheet-->
+	<link rel='stylesheet' type='text/css' href='styles/PersonalityQuiz.css'</link>
+
+<!-- jQuery -->
+    <script src='js/jquery.js'></script>
+
+<!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+	
+</head>
+
+
+
+<body>
+<div class='container'>
+<div id='wrapper'> 
+    <div id='logo'>
+       
+                  
+                    <br>
+                    <br>
+                    <br>
+                      <center><h1><a href='#'>PV Student Community</a></h1></center>       
+					<br>
+					<br>
+                    <br>
+</div>
+	<div id='menu'>
+		<ul>
+			<li class='current_page_item'><a href='index.php'>Home</a></li>
+			<li><a href='login.php'>Login</a></li>
+			<li class ='last'><a href=#>Question/Concerns</a></li>
+		</ul>
+	</div>
+	
+	
+	
+<center>
+<!--BANNER-->
+	<img src='images/PersonalityQuiz.png'>
+			
+
+<!--Questions Progression-->
+
+	
+	<img src='images/0.png'>
+<br>
+<table>
+<tbody>
+<tr>
+<label>Question</label>
+
+<input type='radio' name='quiz_answer[s1][a0]' title class='value-answer' value='5'>
+	<br>
+	<img src='images/25.png'>			
+	
+
+	
+	<img src='images/50.png'>			
+	
+
+		
+	<img src='images/75.png'>			
+	
+		
+</center>
+	
+
+
+
+
+
+   <footer>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-lg-12 text-center'>
+                    <p>Copyright &copy; PV Student Community 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    
+
+
+</center>
+</body>
+</html> ";
+?>
