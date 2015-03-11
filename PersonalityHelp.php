5ec5d8eecf81205027c9b6170f3cfcdc56c8d598
olibia <oligbenjamin@gmail.com>
2015-03-10 20:55:12 -0500

    Personalityhelp page

---
+++ PersonalityHelp.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!doctype html>
+<html>
+<head>
+<title>Register</title>
+<link rel="stylesheet" type="text/css" href="styles/Registration.css"</link>
+</head>
+<body>
+<div class="container">
+<div id="wrapper"> 
+ 
+   <a name="Question1"><h1>How to decide if you're an
+       <br><b>EXTRAVERT</b> or an <b>INTROVERT</b></h1></a>
+   
+   <br>
+   <legend>EXTRAVERT</legend>
+   You get energized by activity, people, variety and talking <br>
+   and when you are at social gatherings and probably prefer <br>
+   to talk to lots of people about a wide range of topics. <br>
+   You like to be in the thick of the action and are eager <br>
+   to share your thoughts and ideas, even if they are not yet<br>
+   fully formed. Some of your best ideas come out during conversations.<br>
+   You usually get energized by being around people and usually look <br>
+   forward to social gatherings. You tend to be outgoing and share <br>
+   information about yourself with others fairly readily.<br>
+   <br>
+   <li>Have high energy</li>
+   <li>Talk more than listen</li>
+   <li>Think out loud</li>
+   <li>Act, then think</li>
+   <li>Like to be around people a lot</li>
+   <li>Prefer a public role</li>
+   <li>Can sometimes be easily distracted</li>
+   <li>Prefer to do lots of things at once</li>
+   <li>Are outgoing & enthusiastic</li>
+<br>
+<legend>INTROVERT</legend>
+
+Your best ideas often come after you've let them "percolate"<br>
+for a while inside your head. While you enjoy some social gatherings,<br>
+you tend to prefer in depth, one-on-one conversations with a few people<br>
+rather than having lots of brief conversations with many different people.<br>
+After spending several hours socializing, you'd probably like to recharge <br>
+your batteries by being alone for a little while. You tend to listen before <br>
+talking and don't like to compete with others for "air time." It may take <br>
+awhile for others to get to know the real you.<br>
+<br>
+<li>Have quiet energy</li>
+<li>Listen more than talk</li>
+<li>Think quietly inside their heads</li>
+<li>Think, then act</li>
+<li>Feel comfortable being alone</li>
+<li>Prefer to work "behind-the-scenes"</li>
+<li>Have good powers of concentration</li>
+<li>Prefer to focus on one thing at a time</li>
+<li>Are self-contained and reserved</li>
+</p>
+</h4>
+<br>
+<br>
+
+
+<a name="Question2"><h1>How to decide if you're a<br>
+    <b>SENSOR</b> or an <b>INTUITIVE</b></h1></a>
+<br>
+<legend>SENSOR</legend>
+
+You tend to be most interested in talking about<br>
+things that are "real", tangible, practical and<br>
+concrete - what you can see, smell, taste, touch<br>
+or hear. You probably pay great attention to <br>
+details and might even be able to relay many recent<br>
+conversations close to verbatim. He said (this)<br> 
+and I then said (that), etc. You tend to tell stories<br>
+with lots of specifics and like others to also give <br>
+you all the pertinent details. You would probably<br> 
+rather implement plans than spend lots of time<br>
+discussing the long-term implications of the plans.<br>
+<br>
+<li>Focus on details & specifics</li>
+<li>Admire practical solutions</li>
+<li>Notice details & remember facts</li>
+<li>Are pragmatic - see what is</li>
+<li>Live in the here-and-now</li>
+<li>Trust actual experience</li>
+<li>Like to use established skills</li>
+<li>Like step-by-step instructions</li>
+<li>Work at a steady pace</li>
+
+<br>
+<legend>INTUITIVE</legend>
+
+You tend to be most interested in ideas and like<br>
+to consider future possibilities. When you are <br>
+having a conversation, your brain is constantly <br>
+making associations and you can easily see<br>
+connections to other ideas and concepts. <br>
+As a result, you often remember your impression <br>
+of conversations and can relay the gist, but not<br>
+the precise wording that was used. You probably <br>
+enjoy brainstorming ideas and seeing long term <br>
+implications and would prefer to be involved in<br>
+creating the vision of what could be rather than<br>
+executing the many steps involved to get there.<br>
+<br>
+<li>Focus on the big picture & possibilities</li>
+<li>Admire creative ideas</li>
+<li>Notice anything new or different</li>
+<li>Are inventive - see what could be</li>
+<li>Think about future implications</li>
+<li>Trust their gut instincts</li>
+<li>Prefer to learn new skills</li>
+<li>Like to figure things out for themselves</li>
+<li>Work in bursts of energy</li>
+<br>
+
+<a name="Question3"><h1>How to decide if you're a
+        <br><b>THINKER</b> or a <b>FEELER</b></h1></a>
+
+
+<legend>THINKER</legend>
+
+You tend to be most comfortable with the part of the <br>
+decision making process that involves logically analyzing<br>
+the pros and cons and looking at the situation as <br>
+objectively as possible, taking the particular feelings <br>
+of individual people out of the equation. Your objective<br>
+is to arrive at a decision that "makes sense" and is <br>
+treats everyone fairly. You probably find it easiest <br>
+when you can be very straightforward with people and <br>
+not have to worry too much about possibly offending <br>
+them with your directness.<br>
+<br>
+<li>Make decisions objectively</li>
+<li>Appear cool and reserved</li>
+<li>Are most convinced by rational arguments</li>
+<li>Are honest and direct</li>
+<li>Value honesty and fairness</li>
+<li>Take few things personally</li>
+<li>Are good at seeing flaws</li>
+<li>Are motivated by achievement</li>
+<li>Argue or debate issues for fun</li>
+
+<br>
+
+<legend>FEELER</legend>
+
+You tend to be most comfortable in the part of the<br>
+decision making process that involves focusing on <br>
+how the people involved will be impacted. You tend<br>
+to decide based on how you and others will feel about<br>
+the situation and have a strong need to be true to your <br>
+own personal values. A top priority for you is maintaining<br>
+harmonious relationships and avoiding tense, <br>
+conflict-ridden situations. You may be very sensitive<br>
+to the emotional undertones of any interaction and <br>
+probably work to avoid others having hard feelings <br>
+or not feeling valued.<br>
+<br>
+<li>Decide based on their values & feelings</li>
+<li>Appear warm and friendly</li>
+<li>Are most convinced by how they feel</li>
+<li>Are diplomatic and tactful</li>
+<li>Value harmony and compassion</li>
+<li>Take many things personally</li>
+<li>Are quick to compliment others</li>
+<li>Are motivated by appreciation</li>
+<li>Avoid arguments and conflicts</li>
+    
+ <br>
+ <br>
+ 
+<a name="Question4"> <h1>How to decide if you're a
+        <br><b> JUDGER</b> or a <b>PERCEIVER</b></h1></a>
+ 
+ <legend>JUDGER</legend>
+
+ You tend to be driven towards closure whether<br>
+making a decision, making a plan or completing<br>
+a project, and may feel uncomfortable when things <br>
+are too unstructured or left undecided. <br>
+You probably have two separate modes: "play"<br>
+mode and "on-task" mode. When you are "on-task"<br>
+you can seem rather serious and get stressed <br>
+since it's important to get your tasks accomplished<br>
+on time. You may work for long periods of time <br>
+without taking a break especially when you are<br>
+close to finishing a project. You probably have<br>
+a to-do list and derive satisfaction from checking<br>
+off completed items.<br>
+<br>
+<li>Like to have things settled</li>
+<li>Take responsibilities seriously</li>
+<li>Pay attention to time & are usually prompt</li>
+<li>Prefer to finish projects</li>
+<li>Work first, play later</li>
+<li>Seek closure</li>
+<li>See the need for most rules</li>
+<li>Like to make & stick with plans</li>
+<li>Find comfort in schedules</li>
+
+<br>
+<legend>PERCEIVER</legend>
+
+You like to keep your options open and take in lots<br>
+of information before making a decision. You enjoy <br>
+being spontaneous in your social life. <br>
+You generally prefer an informal and casual approach<br>
+to most things and probably don't like a lot of<br>
+unnecessary rules. While you've learned to meet most<br>
+deadlines, you may do your best work when you get <br>
+the adrenaline rush at the last minute. You might <br>
+like to take frequent breaks especially when working<br>
+on lengthy projects.<br>
+<br>
+<li>Like to keep their options open</li>
+<li>Are playful and casual</li>
+<li>Are less aware of time and may run late</li>
+<li>Prefer to start projects</li>
+<li>Play first, work later</li>
+<li>May have difficulty making some decisions</li>
+<li>Question the need for many rules</li>
+<li>Like to keep plans flexible</li>
+<li>Want the freedom to be spontaneous</li>
+
+    <br>
+    <br>
+    
+    <br>
+    <br>
+    <br>
+    <!-- Student Button START -->
+    <h5> source:  http://www.personalitytype.com/career_quiz</h5>  
+                
+    <center><form><h4> 
+                
+                
+    
+   
+ </div>
+</div>
+</div>
+</body>
+</html>
