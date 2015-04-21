1d550f80d38e8e722a03d3c6e98293bb7cfc7d6d
jalisap <jalisaperkins10@gmail.com>
2015-04-21 15:50:14 -0500

    Suggested Users
    This is the suggested users page and will hold 5 suggested users in the
    table.

---
+++ suggestedusers.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+<center> <link rel='stylesheet' type='text/css' href='styles/FAQs.css'</link>
+
+<!-- jQuery -->
+    <script src='js/jquery.js'></script>
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<div class='container'>
+<div id='wrapper'> 
+    <div id='logo'>
+       
+                  
+                    <br>
+                    <br>
+                    <br>
+                      <center><h1><a href='index.php'>PV Student Community</a></h1></center>       
+					<br>
+					<br>
+                    <br>
+	</div>
+	<div id='menu'>
+		<ul>
+			<li class='current_page_item'><a href='index.php'>Home</a></li>
+			<li><a href='login.php'>Login</a></li>
+			<li class ='last'><a href=#>Question/Concerns</a></li>
+		</ul>
+	</div>
+	<center>
+			<br>
+			<img src='images/mhmm.png'>
+			
+		
+<table style="width:50%">
+  <tr>
+    <td>Student Name 1 *to be extracted*</td>
+	<td>Gender: *to be extracted using students primary key from database*</td>
+  </tr>
+  <tr>
+	<td>Student Picture</td>
+	<td>Classification: *to be extracted using students primary key from database*</td> 
+  </tr>
+</table>	
+<br>
+<table style="width:50%">
+  <tr>
+	 <td>Student Name 2*to be extracted*</td>
+	 <td>Gender: *to be extracted using students primary key from database*</td>
+	
+  </tr>
+  <tr>
+	<td>Student Picture</td>
+    <td>Classification: *to be extracted using students primary key from database*</td> 
+  </tr>
+</table>	
+<br>
+<table style="width:50%">
+  <tr>
+    <td>Student Name 3 *to be extracted*</td>
+	<td>Gender: *to be extracted using students primary key from database*</td>
+  </tr>
+  <tr>
+    <td>Student Picture</td>
+    <td>Classification: *to be extracted using students primary key from database*</td> 
+  </tr>
+</table>	
+<br>
+<table style="width:50%">
+  <tr>
+    <td>Student Name 4 *to be extracted*</td>
+	<td>Gender: *to be extracted using students primary key from database*</td>
+  </tr>
+  <tr>
+    <td>Student Picture</td>
+    <td>Classification: *to be extracted using students primary key from database*</td> 
+  </tr>
+</table>	
+<br>
+<table style="width:50%">
+  <tr>
+    <td>Student Name 5 *to be extracted*</td>
+	<td>Gender: *to be extracted using students primary key from database*</td>
+  </tr>
+  <tr>
+    <td>Student Picture</td>
+    <td>Classification: *to be extracted using students primary key from database*</td> 
+  </tr>
+</table>	
+<br>
+
+</div>
+</div>
+
+   <footer>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-lg-12 text-center'>
+                    <p>Copyright &copy; PV Student Community 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    
+
+    <!-- Bootstrap Core JavaScript 
+    <script src='js/bootstrap.min.js'></script>-->
+</center>
+<?php
+
+
+?>
+</body>
+</html> 
